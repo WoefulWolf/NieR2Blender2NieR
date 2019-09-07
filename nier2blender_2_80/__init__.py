e32800f3d41bc5a576ee4c1eb6049c9a6e15c4b8
Woeful_Wolf <Woeful_Wolf@users.noreply.github.com>
2019-09-08 00:19:22 +0200

    Added automatic dtt/dat extraction and importing as an option

--- nier2blender_2_80/__init__.py
+++ nier2blender_2_80/__init__.py
@@ -1,11 +1,11 @@
 bl_info = {
-    "name": "Nier: Automata model importer",
+    "name": "Nier: Automata Model Importer",
     "author": "C4nf3ng (2.80 by Woeful_Wolf)",
     "version": (1, 1),
     "blender": (2, 80, 0),
     "api": 38019,
     "location": "File > Import-Export",
-    "description": "Import Nier:Automata wmb model data",
+    "description": "Import Nier:Automata Model Data",
     "warning": "",
     "wiki_url": "",
     "tracker_url": "",
@@ -14,11 +14,12 @@ bl_info = {
 #just for Break
 
 import bpy
+import os
 from bpy_extras.io_utils import ExportHelper,ImportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 
 class ImportNier2blender(bpy.types.Operator, ImportHelper):
-    '''Load a Nier: Automata WMB File.'''
+    '''Load a Nier:Automata WMB File.'''
     bl_idname = "import.wmb_data"
     bl_label = "Import WMB Data"
     bl_options = {'PRESET'}
@@ -29,20 +30,53 @@ class ImportNier2blender(bpy.types.Operator, ImportHelper):
         from nier2blender_2_80 import wmb_importer
         return wmb_importer.main(self.filepath)
 
+class ImportDATNier2blender(bpy.types.Operator, ImportHelper):
+    '''Load a Nier:Automata DTT (and DAT) File.'''
+    bl_idname = "import.dtt_data"
+    bl_label = "Import DTT (and DAT) Data"
+    bl_options = {'PRESET'}
+    filename_ext = ".dtt"
+    filter_glob: StringProperty(default="*.dtt", options={'HIDDEN'})
+
+    def execute(self, context):
+        head = os.path.split(self.filepath)[0]
+        tail = os.path.split(self.filepath)[1]
+        tailless_tail = tail[:-4]
+        dat_filepath = head + '/' + tailless_tail + '.dat'
+        extract_dir = head + '/nier2blender_extracted'
+        from nier2blender_2_80 import dat_unpacker
+        if os.path.isfile(dat_filepath):
+            dat_unpacker.main(dat_filepath, extract_dir + '/' + tailless_tail + '.dat', dat_filepath)   # dat
+        else:
+            print('DAT not found. Only extracting DTT. (No materials will automatically be imported)')
+        dat_unpacker.main(self.filepath, extract_dir + '/' + tailless_tail + '.dtt', self.filepath)       # dtt
+
+        wmb_filepath = extract_dir + '/' + tailless_tail + '.dtt/' + tailless_tail + '.wmb'
+        print(self.filepath)
+        print(dat_filepath)
+        print(wmb_filepath)
+        from nier2blender_2_80 import wmb_importer
+        return wmb_importer.main(wmb_filepath)
 
 # Registration
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportNier2blender.bl_idname, text="WMB File for Nier: Automata (.wmb)")
+    self.layout.operator(ImportNier2blender.bl_idname, text="WMB File for Nier:Automata (.wmb)")
 
+def menu_func_import_dat(self, context):
+    self.layout.operator(ImportDATNier2blender.bl_idname, text="DTT File for Nier:Automata (.dtt)")
 
 def register():
     bpy.utils.register_class(ImportNier2blender)
+    bpy.utils.register_class(ImportDATNier2blender)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import_dat)
 
 def unregister():
     bpy.utils.unregister_class(ImportNier2blender)
+    bpy.utils.unregister_class(ImportDATNier2blender)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import_dat)
 
 
 if __name__ == '__main__':
