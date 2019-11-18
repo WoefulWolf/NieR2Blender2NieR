3e43f73f082bf9a6859aee1ba629244b51a69d62
WoefulWolf <Woeful_Wolf@users.noreply.github.com>
2019-11-18 18:46:10 +0200

    corrected index

--- blender2nier/batches/batch.py
+++ blender2nier/batches/batch.py
@@ -7,5 +7,5 @@ class c_batch(object):
         self.vertexStart = 0
         self.indexStart = indexStart
         self.numVertexes = len(obj.data.vertices) + prev_numVertexes
-        self.numIndexes = len(obj.data.polygons) * 3
+        self.numIndexes = len(obj.data.loops)
         self.numPrimitives = len(obj.data.polygons)
