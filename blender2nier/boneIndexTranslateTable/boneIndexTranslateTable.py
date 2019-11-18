142e1866f62665ad7b57d5865374a7e7db320e5f
WoefulWolf <Woeful_Wolf@users.noreply.github.com>
2019-11-18 18:46:43 +0200

    added correct boneIndexTranslateTable according to new import

--- blender2nier/boneIndexTranslateTable/boneIndexTranslateTable.py
+++ blender2nier/boneIndexTranslateTable/boneIndexTranslateTable.py
@@ -3,59 +3,26 @@ import bpy, bmesh, math
 class c_boneIndexTranslateTable(object):
     def __init__(self, bones):
 
-        def get_firstLevel(self, bones):
-            firstLevel_Size = 16               # I 'think' firstLevel is always 16
-            firstLevel = [-1] * firstLevel_Size
-                           
+        self.firstLevel = []
+        self.secondLevel = []
+        self.thirdLevel = []
 
-            for bone in bones.bones:
-                print(bone[0])
-                firstLevel[0] = 16
-            
-            print(firstLevel)
-            return firstLevel
+        for obj in bpy.data.objects:
+            if obj.type == 'ARMATURE':
+                for idx in range(len(obj.data['firstLevel'])):
+                    self.firstLevel.append(obj.data['firstLevel'][idx])
 
-        self.firstLevel_Size = 16
-        self.firstLevel = get_firstLevel(self, bones)
+                for idx in range(len(obj.data['secondLevel'])):
+                    self.secondLevel.append(obj.data['secondLevel'][idx])
+
+                for idx in range(len(obj.data['thirdLevel'])):
+                    self.thirdLevel.append(obj.data['thirdLevel'][idx])
+
+        self.firstLevel_Size = len(self.firstLevel)
+
+        self.secondLevel_Size = len(self.secondLevel)   
+
+        self.thirdLevel_Size = len(self.thirdLevel)
 
 
-        def get_secondLevel_Size(self):
-            secondLevel_Size = 0
-            for firstLevel in self.firstLevel:
-                if firstLevel != -1:
-                    secondLevel_Size += 1
-            return secondLevel_Size * 16
-
-        def get_secondLevel(self):
-            secondLevel = [-1] * get_secondLevel_Size(self)
-
-            secondLevel[0] = 32             # THIS IS NOT LEGIT, GOT FROM BINARY NEEDS TO BE CHANGED PER MODEL TYPE
-            secondLevel[1] = 48             # THIS IS NOT LEGIT, GOT FROM BINARY NEEDS TO BE CHANGED PER MODEL TYPE
-            secondLevel[2] = 64             # THIS IS NOT LEGIT, GOT FROM BINARY NEEDS TO BE CHANGED PER MODEL TYPE
-            
-            print(secondLevel)
-            return secondLevel
-
-        self.secondLevel_Size = get_secondLevel_Size(self)
-        self.secondLevel = get_secondLevel(self)
-        
-
-        def get_thirdLevel_Size(self):
-            thirdLevel_Size = 0
-            for secondLevel in self.secondLevel:
-                if secondLevel != -1:
-                    thirdLevel_Size += 1
-            return thirdLevel_Size * 16
-
-        def get_thirdLevel(self):
-            thirdLevel = [4095] * get_thirdLevel_Size(self)
-            
-            for bone in bones.bones:
-                thirdLevel[bone[0]] = int(bone[-1][-1])
-
-            print(thirdLevel)
-            return thirdLevel
-
-        self.thirdLevel_Size = get_thirdLevel_Size(self)
-        self.thirdLevel = get_thirdLevel(self)
         self.boneIndexTranslateTable_StructSize = 16*2 + self.secondLevel_Size*2 + self.thirdLevel_Size*2
