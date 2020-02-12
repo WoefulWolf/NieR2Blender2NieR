bed0792dea655248574fe86814eef63e9e532524
WoefulWolf <54476280+WoefulWolf@users.noreply.github.com>
2020-02-12 22:41:43 +0200

    Hotfix: Added try/except to Bulk import, this way it will continue with the rest even if it encounters an invalid DTT/DAT

--- nier2blender_2_80/__init__.py
+++ nier2blender_2_80/__init__.py
@@ -51,25 +51,28 @@ class ImportDATNier2blender(bpy.types.Operator, ImportHelper):
             folder = os.path.split(self.filepath)[0]
             for filename in os.listdir(folder):
                 if filename[-4:] == '.dtt':
-                    filepath = folder + '\\' + filename
-                    head = os.path.split(filepath)[0]
-                    tail = os.path.split(filepath)[1]
-                    tailless_tail = tail[:-4]
-                    dat_filepath = head + '\\' + tailless_tail + '.dat'
-                    extract_dir = head + '\\nier2blender_extracted'
-                    from nier2blender_2_80 import dat_unpacker
-                    if os.path.isfile(dat_filepath):
-                        dat_unpacker.main(dat_filepath, extract_dir + '\\' + tailless_tail + '.dat', dat_filepath)   # dat
-                    else:
-                        print('DAT not found. Only extracting DTT. (No materials will automatically be imported)')
+                    try:
+                        filepath = folder + '\\' + filename
+                        head = os.path.split(filepath)[0]
+                        tail = os.path.split(filepath)[1]
+                        tailless_tail = tail[:-4]
+                        dat_filepath = head + '\\' + tailless_tail + '.dat'
+                        extract_dir = head + '\\nier2blender_extracted'
+                        from nier2blender_2_80 import dat_unpacker
+                        if os.path.isfile(dat_filepath):
+                            dat_unpacker.main(dat_filepath, extract_dir + '\\' + tailless_tail + '.dat', dat_filepath)   # dat
+                        else:
+                            print('DAT not found. Only extracting DTT. (No materials will automatically be imported)')
 
-                    wtp_filename = dat_unpacker.main(filepath, extract_dir + '\\' + tailless_tail + '.dtt', filepath)       # dtt
+                        wtp_filename = dat_unpacker.main(filepath, extract_dir + '\\' + tailless_tail + '.dtt', filepath)       # dtt
 
-                    wmb_filepath = extract_dir + '\\' + tailless_tail + '.dtt\\' + wtp_filename[:-4] + '.wmb'
-                    if not os.path.exists(wmb_filepath):
-                        wmb_filepath = extract_dir + '\\' + tailless_tail + '.dat\\' + wtp_filename[:-4] + '.wmb'                     # if not in dtt, then must be in dat
+                        wmb_filepath = extract_dir + '\\' + tailless_tail + '.dtt\\' + wtp_filename[:-4] + '.wmb'
+                        if not os.path.exists(wmb_filepath):
+                            wmb_filepath = extract_dir + '\\' + tailless_tail + '.dat\\' + wtp_filename[:-4] + '.wmb'                     # if not in dtt, then must be in dat
 
-                    wmb_importer.main(wmb_filepath)
+                        wmb_importer.main(wmb_filepath)
+                    except:
+                        print('ERROR: FAILED TO IMPORT', filename)
             return {'FINISHED'}
 
         else:
