a829dca1eed0693054f7958555928aa0088f0b06
Arthur Heitmann <art.tec.15@gmail.com>
2022-03-23 13:28:35 +0100

    maybe huuuge performance boost + perf debugging

--- vertexGroups/vertexGroup.py
+++ vertexGroups/vertexGroup.py
@@ -2,6 +2,9 @@ import bpy, bmesh, math, mathutils
 import numpy as np
 import math
 from ..util import Vector3
+import time
+from timeit import default_timer as timer
+
 
 class c_vertexGroup(object):
     def __init__(self, vertexGroupIndex, vertexesStart):
@@ -24,7 +27,7 @@ class c_vertexGroup(object):
             return blenderObjects
         
         self.blenderObjects = get_blenderObjects(self)
-
+        
         def get_numVertices(self):
             numVertices = 0
             for obj in self.blenderObjects:
@@ -108,7 +111,7 @@ class c_vertexGroup(object):
             self.vertexExDataSize = 16
         elif self.vertexFlags == 11:
             self.vertexExDataSize = 20
-        
+
 
         def get_boneMap(self):
             boneMap = []
@@ -133,36 +136,65 @@ class c_vertexGroup(object):
         def get_vertexesData(self):
             vertexes = []
             vertexesExData = []
+            totals = [0]*15
+            totalT1 = timer()
             for bvertex_obj in blenderVertices:
                 print('   [>] Generating vertex data for object', bvertex_obj[1].name)
+                t1 = timer()
                 loops = get_blenderLoops(self, bvertex_obj[1])
                 sorted_loops = sorted(loops, key=lambda loop: loop.vertex_index)
 
-                if self.vertexFlags not in [0, 1, 4, 5, 12, 14]:
+                if not(self.vertexFlags == 0 or self.vertexFlags == 1 or self.vertexFlags == 4 or self.vertexFlags == 5 or self.vertexFlags == 12 or self.vertexFlags == 14):
                     boneSet = get_boneSet(self, bvertex_obj[1]["boneSetIndex"])
+                    # print("   ", boneSet, sep="")
 
-                added_indices = []
+                totals[0] += timer() - t1
+                t1 = timer()
+
+                # added_indices = []
+                previousIndex = -1
                 for loop in sorted_loops:
-                    if loop.vertex_index in added_indices:
+                    t1 = timer()
+                    
+                    # print(loop.vertex_index, "", end="")
+
+                    if loop.vertex_index == previousIndex:
+                        totals[9] += timer() - t1
+                        # print("- ", end="")
                         continue
-                    added_indices.append(loop.vertex_index)
+
+                    previousIndex = loop.vertex_index
+            
+                    totals[8] += timer() - t1
+                    t1 = timer()
+
+                    # added_indices.append(loop.vertex_index)
 
                     bvertex = bvertex_obj[0][loop.vertex_index]
                     # XYZ Position
-                    position = Vector3(round(bvertex.co.x, 6), round(bvertex.co.y, 6), round(bvertex.co.z, 6))
+                    position = [round(bvertex.co.x, 6), round(bvertex.co.y, 6), round(bvertex.co.z, 6)]
+
+                    totals[5] += timer() - t1
+                    t1 = timer()
             
                     # Tangents
-                    tx = np.clip(round(loop.tangent[0]*127.0+127.0), 0, 255)
-                    ty = np.clip(round(loop.tangent[1]*127.0+127.0), 0, 255)
-                    tz = np.clip(round(loop.tangent[2]*127.0+127.0), 0, 255)
-                    sign = np.clip(round(-loop.bitangent_sign*127.0+128.0), 0, 255)
+                    tx = clamp(round(loop.tangent[0]*127.0+127.0), 0, 255)
+                    ty = clamp(round(loop.tangent[1]*127.0+127.0), 0, 255)
+                    tz = clamp(round(loop.tangent[2]*127.0+127.0), 0, 255)
+                    sign = clamp(round(-loop.bitangent_sign*127.0+128.0), 0, 255)
 
                     tangents = [tx, ty, tz, sign]
 
+                    totals[6] += timer() - t1
+                    t1 = timer()
+
                     # Normal
                     normal = []
-                    if self.vertexFlags in [0]:
+                    if self.vertexFlags == 0:
                         normal = [loop.normal[0], loop.normal[1], loop.normal[2], 0]
+
+                    totals[7] += timer() - t1
+                    t1 = timer()
 
                     # UVs
                     uv_maps = []
@@ -174,10 +206,13 @@ class c_vertexGroup(object):
                         uv2 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv2)
 
+                    totals[1] += timer() - t1
+
                     # Bones
                     boneIndexes = []
                     boneWeights = []
-                    if self.vertexFlags in [7, 10, 11]:
+                    if self.vertexFlags == 7 or self.vertexFlags == 10 or self.vertexFlags == 11:
+                        t1 = timer()
                         # Bone Indices
                         for groupRef in bvertex.groups:
                             if len(boneIndexes) < 4:
@@ -192,11 +227,15 @@ class c_vertexGroup(object):
                         if len(boneIndexes) == 0:
                             print(len(vertexes) ,"- Vertex Weights Error: Vertex has no assigned groups. At least 1 required. Try using Blender's [Select -> Select All By Trait > Ungrouped Verts] function to find them.")
 
+                        totals[11] += timer() - t1
+                        t1 = timer()
+
                         while len(boneIndexes) < 4:
                             boneIndexes.append(0)
-
+                        
                         # Bone Weights
                         weights = [group.weight for group in bvertex.groups]
+                        weightsSum = sum(weights)
 
                         if len(weights) >  4:
                             print(len(vertexes), "- Vertex Weights Error: Vertex has weights assigned to more than 4 groups. Try using Blender's [Weights -> Limit Total] function.")
@@ -204,9 +243,12 @@ class c_vertexGroup(object):
                         normalized_weights = []                                             # Force normalize the weights as Blender's normalization sometimes get some rounding issues.
                         for val in weights:
                             if val > 0:
-                                normalized_weights.append(float(val)/sum(weights))
+                                normalized_weights.append(float(val)/weightsSum)
                             else:
                                 normalized_weights.append(0)
+
+                        totals[12] += timer() - t1
+                        t1 = timer()
 
                         for val in normalized_weights:
                             if len(boneWeights) < 4:
@@ -215,6 +257,9 @@ class c_vertexGroup(object):
                                     weight = 255
                                 boneWeights.append(weight)
                         
+                        totals[13] += timer() - t1
+                        t1 = timer()
+
                         while len(boneWeights) < 4:
                             boneWeights.append(0)
 
@@ -227,6 +272,10 @@ class c_vertexGroup(object):
                         if sum(boneWeights) != 255:                       # If EVEN the FORCED normalization doesn't work, say something :/
                             print(len(vertexes), "- Vertex Weights Error: Vertex has a total weight not equal to 1.0. Try using Blender's [Weights -> Normalize All] function.") 
 
+                        totals[14] += timer() - t1
+                    
+                    t1 = timer()
+
                     color = []
                     if self.vertexFlags in [4, 5, 12, 14]:
                         if len (bvertex_obj[1].data.vertex_colors) == 0:
@@ -235,8 +284,11 @@ class c_vertexGroup(object):
                         loop_color = bvertex_obj[1].data.vertex_colors.active.data[loop.index].color
                         color = [round(loop_color[0]*255), round(loop_color[1]*255), round(loop_color[2]*255), round(loop_color[3]*255)]
 
-                    vertexes.append([position.xyz, tangents, normal, uv_maps, boneIndexes, boneWeights, color])
+                    vertexes.append([position, tangents, normal, uv_maps, boneIndexes, boneWeights, color])
 
+                    
+                    totals[3] += timer() - t1
+                    t1 = timer()
 
                     ##################################################
                     ###### Now lets do the extra data shit ###########
@@ -244,30 +296,29 @@ class c_vertexGroup(object):
                     normal = []
                     uv_maps = []
                     color = []
-
-                    if self.vertexFlags in [10, 11]:
+                    if self.vertexFlags == 10 or self.vertexFlags == 11:
                         if len (bvertex_obj[1].data.vertex_colors) == 0:
                             print("Object had no vertex colour layer when one was expected - creating one.")
                             new_vertex_colors = bvertex_obj[1].data.vertex_colors.new()
 
-                    if self.vertexFlags in [1, 4, 5, 7, 10, 11, 12, 14]:
+                    if self.vertexFlags == 1 or self.vertexFlags == 4 or self.vertexFlags == 5 or self.vertexFlags == 7 or self.vertexFlags == 10 or self.vertexFlags == 11 or self.vertexFlags == 12 or self.vertexFlags == 14:
                         normal = [loop.normal[0], loop.normal[1], loop.normal[2], 0]
                     
-                    if self.vertexFlags in [5]:
+                    if self.vertexFlags == 5:
                         uv3 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 2)
                         uv_maps.append(uv3)
 
-                    elif self.vertexFlags in [7]:
+                    elif self.vertexFlags == 7:
                         uv2 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv2)
 
-                    elif self.vertexFlags in [10]:
+                    elif self.vertexFlags == 10:
                         uv2 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv2)
                         loop_color = bvertex_obj[1].data.vertex_colors.active.data[loop.index].color
                         color = [round(loop_color[0]*255), round(loop_color[1]*255), round(loop_color[2]*255), round(loop_color[3]*255)]
 
-                    elif self.vertexFlags in [11]:
+                    elif self.vertexFlags == 11:
                         uv2 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv2)
                         loop_color = bvertex_obj[1].data.vertex_colors.active.data[loop.index].color
@@ -275,7 +326,7 @@ class c_vertexGroup(object):
                         uv3 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv3)
 
-                    elif self.vertexFlags in [12]:
+                    elif self.vertexFlags == 12:
                         uv2 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 1)
                         uv_maps.append(uv2)
                         uv3 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 2)
@@ -285,15 +336,18 @@ class c_vertexGroup(object):
                         uv5 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 4)
                         uv_maps.append(uv5)
 
-                    elif self.vertexFlags in [14]:
+                    elif self.vertexFlags == 14:
                         uv3 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 2)
                         uv_maps.append(uv3)
                         uv4 = get_blenderUVCoords(self, bvertex_obj[1], loop.index, 3)
                         uv_maps.append(uv4)
+                    
+                    totals[4] += timer() - t1
+                    t1 = timer()
 
                     vertexExData = [normal, uv_maps, color]
                     vertexesExData.append(vertexExData)
-
+            print(totals, timer() - totalT1)
             return vertexes, vertexesExData
 
         def get_indexes(self):
@@ -331,7 +385,11 @@ class c_vertexGroup(object):
         self.numIndexes = get_numIndexes(self)
 
         self.vertexes, self.vertexesExData = get_vertexesData(self)
-
+        
         self.indexes = get_indexes(self)
 
         self.vertexGroupSize = (self.numVertexes * self.vertexSize) + (self.numVertexes * self.vertexExDataSize) + (self.numIndexes * 4)
+
+def clamp(val, minV, maxV):
+    return maxV if val > maxV else \
+        minV if val < minV else val
--- wmb/wmb_vertexGroups.py
+++ wmb/wmb_vertexGroups.py
@@ -1,8 +1,16 @@
 from ..util import *
+from timeit import default_timer as timer
 
 def create_wmb_vertexGroups(wmb_file, data):
+    totals = [0]*5
+    totalT1 = timer()
+    t1 = timer()
+
     wmb_file.seek(data.vertexGroups_Offset)
 
+    totals[0] += timer() - t1
+    t1 = timer()
+    
     for vertexGroup in data.vertexGroups.vertexGroups:
         write_uInt32(wmb_file, vertexGroup.vertexOffset)            # vertexOffset
         write_uInt32(wmb_file, vertexGroup.vertexExDataOffset)      # vertexExDataOffset
@@ -17,51 +25,58 @@ def create_wmb_vertexGroups(wmb_file, data):
         write_Int32(wmb_file, vertexGroup.indexBufferOffset)        # indexBufferOffset
         write_Int32(wmb_file, vertexGroup.numIndexes)               # numIndexes
 
+    totals[1] += timer() - t1
+    t1 = timer()
+
     for vertexGroup in data.vertexGroups.vertexGroups:
+        t1 = timer()
         for vertex in vertexGroup.vertexes:                         # [position.xyz, tangents, normal, uv_maps, boneIndexes, boneWeights, color]
             write_xyz(wmb_file, vertex[0])                          # position.xyz
             for val in vertex[1]:                                   # tangents
                 write_byte(wmb_file, val)
             for val in vertex[3][0]:                                # UVMap 1
                 write_float16(wmb_file, val)
-            if vertexGroup.vertexFlags in [0]:                      # Normal
+            if vertexGroup.vertexFlags == 0:                      # Normal
                 for val in vertex[2]:
                     write_float16(wmb_file, val)
-            if vertexGroup.vertexFlags in [1, 4, 5, 12, 14]:
+            if vertexGroup.vertexFlags == 1 or vertexGroup.vertexFlags == 4 or vertexGroup.vertexFlags == 5 or vertexGroup.vertexFlags == 12 or vertexGroup.vertexFlags == 14:
                 for val in vertex[3][1]:                            # UVMap 2
                     write_float16(wmb_file, val)
-            if vertexGroup.vertexFlags in [7, 10, 11]:
+            if vertexGroup.vertexFlags == 7 or vertexGroup.vertexFlags == 10 or vertexGroup.vertexFlags == 11:
                 for val in vertex[4]:                               
                     write_byte(wmb_file, val)                       # Bone Indices
                 for val in vertex[5]:                                   
                     write_byte(wmb_file, val)                       # Bone Weights
-            if vertexGroup.vertexFlags in [4, 5, 12, 14]:
+            if vertexGroup.vertexFlags == 4 or vertexGroup.vertexFlags == 5 or vertexGroup.vertexFlags == 12 or vertexGroup.vertexFlags == 14:
                 for val in vertex[6]:                                   
                     write_byte(wmb_file, val)                       # Color
 
+        totals[2] += timer() - t1
+        t1 = timer()
+
         wmb_file.seek(vertexGroup.vertexExDataOffset)
         for vertexExData in vertexGroup.vertexesExData:             # [normal, uv_maps, color]
-            if vertexGroup.vertexFlags in [1, 4]:                   # [1, 4]
+            if vertexGroup.vertexFlags == 1 or vertexGroup.vertexFlags == 4:                   # [1, 4]
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [5]:                    # [5]
+            elif vertexGroup.vertexFlags == 5:                    # [5]
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
                 for val in vertexExData[1][0]:                      # UVMap 3
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [7]:                    # [7]
+            elif vertexGroup.vertexFlags == 7:                    # [7]
                 for val in vertexExData[1][0]:                      # UVMap 1
                     write_float16(wmb_file, val)
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [10]:                   # [10]
+            elif vertexGroup.vertexFlags == 10:                   # [10]
                 for val in vertexExData[1][0]:                      # UVMap 1
                     write_float16(wmb_file, val)
                 for val in vertexExData[2]:                         # Color                               
                     write_byte(wmb_file, val)
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [11]:                   # [11]
+            elif vertexGroup.vertexFlags == 11:                   # [11]
                 for val in vertexExData[1][0]:                      # UVMap 1
                     write_float16(wmb_file, val)
                 for val in vertexExData[2]:                         # Color                                   
@@ -70,7 +85,7 @@ def create_wmb_vertexGroups(wmb_file, data):
                     write_float16(wmb_file, val)
                 for val in vertexExData[1][1]:                      # UVMap 2
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [12]:                   # [12]
+            elif vertexGroup.vertexFlags == 12:                   # [12]
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
                 for val in vertexExData[1][0]:                      # UVMap 3
@@ -79,7 +94,7 @@ def create_wmb_vertexGroups(wmb_file, data):
                     write_float16(wmb_file, val)
                 for val in vertexExData[1][2]:                      # UVMap 5
                     write_float16(wmb_file, val)
-            elif vertexGroup.vertexFlags in [14]:                   # [14]
+            elif vertexGroup.vertexFlags == 14:                   # [14]
                 for val in vertexExData[0]:                         # normal
                     write_float16(wmb_file, val)
                 for val in vertexExData[1][0]:                      # UVMap 3
@@ -87,5 +102,12 @@ def create_wmb_vertexGroups(wmb_file, data):
                 for val in vertexExData[1][1]:                      # UVMap 4
                     write_float16(wmb_file, val)
 
+        totals[3] += timer() - t1
+        t1 = timer()
+        
         for index in vertexGroup.indexes:                           # indexes
             write_uInt32(wmb_file, index)
+        
+        totals[4] += timer() - t1
+    
+    print(totals, timer() - totalT1)
