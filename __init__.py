5f62957e754b868085187c02d3f71881d0770fc1
ArthurHeitmann <art.tec.15@gmail.com>
2022-06-03 21:18:29 +0200

    Added separate dat (col+lay) importer

--- __init__.py
+++ __init__.py
@@ -101,6 +101,63 @@ class ImportDATNier2blender(bpy.types.Operator, ImportHelper):
         else:
             return importDat(self.only_extract, self.filepath)
 
+class ImportActualDATNier2blender(bpy.types.Operator, ImportHelper):
+    '''Load a Nier:Automata DAT File.'''
+    bl_idname = "import_scene.dat_data"
+    bl_label = "Import DAT Data"
+    bl_options = {'PRESET'}
+    filename_ext = ".dat"
+    filter_glob: StringProperty(default="*.dat", options={'HIDDEN'})
+
+    reset_blend: bpy.props.BoolProperty(name="Reset Blender Scene on Import", default=True)
+    bulk_import: bpy.props.BoolProperty(name="Bulk Import All DTT/DATs In Folder (Experimental)", default=False)
+    only_extract: bpy.props.BoolProperty(name="Only Extract DTT/DAT Contents. (Experimental)", default=False)
+
+    def doImport(self, onlyExtract, filepath):
+        head = os.path.split(filepath)[0]
+        tail = os.path.split(filepath)[1]
+        tailless_tail = tail[:-4]
+        dat_filepath = head + '\\' + tailless_tail + '.dat'
+        extract_dir = head + '\\nier2blender_extracted'
+        from . import dat_unpacker
+        if os.path.isfile(dat_filepath):
+            dat_unpacker.main(dat_filepath, extract_dir + '\\' + tailless_tail + '.dat', dat_filepath)   # dat
+
+        if onlyExtract:
+            return {'FINISHED'}
+
+        # COL
+        col_filepath = extract_dir + '\\' + tailless_tail + '.dat\\' + tailless_tail + '.col'
+        if os.path.isfile(col_filepath):
+            from . import col_importer
+            col_importer.main(col_filepath)
+
+        # LAY
+        lay_filepath = extract_dir + '\\' + tailless_tail + '.dat\\' + 'Layout.lay'
+        if os.path.isfile(lay_filepath):
+            from . import lay_importer
+            lay_importer.main(lay_filepath, __name__)
+
+        return {'FINISHED'}
+
+    def execute(self, context):
+        from . import wmb_importer
+        if self.reset_blend and not self.only_extract:
+            wmb_importer.reset_blend()
+        if self.bulk_import:
+            folder = os.path.split(self.filepath)[0]
+            for filename in os.listdir(folder):
+                if filename[-4:] == '.dat':
+                    try:
+                        filepath = folder + '\\' + filename
+                        return self.doImport(self.only_extract, filepath)
+                    except:
+                        print('ERROR: FAILED TO IMPORT', filename)
+            return {'FINISHED'}
+
+        else:
+            return self.doImport(self.only_extract, self.filepath)
+
 class ImportColNier2Blender(bpy.types.Operator, ImportHelper):
     '''Load a Nier:Automata Col (Collision) File.'''
     bl_idname = "import_scene.col_data"
@@ -198,12 +255,14 @@ def menu_func_import(self, context):
     yorha_icon = pcoll["yorha"]
     self.layout.operator(ImportDATNier2blender.bl_idname, text="DTT File for Nier:Automata (.dtt)", icon_value=yorha_icon.icon_id)
     self.layout.operator(ImportNier2blender.bl_idname, text="WMB File for Nier:Automata (.wmb)", icon_value=yorha_icon.icon_id)
+    self.layout.operator(ImportActualDATNier2blender.bl_idname, text="DAT File for Nier:Automata (col+lay) (.dat)", icon_value=yorha_icon.icon_id)
     self.layout.operator(ImportColNier2Blender.bl_idname, text="Collision File for Nier:Automata (.col)", icon_value=yorha_icon.icon_id)
     self.layout.operator(ImportLayNier2Blender.bl_idname, text="Layout File for Nier:Automata (.lay)", icon_value=yorha_icon.icon_id)
 
 classes = (
     ImportNier2blender,
     ImportDATNier2blender,
+    ImportActualDATNier2blender,
     ImportColNier2Blender,
     ImportLayNier2Blender,
     SelectDirectory,
