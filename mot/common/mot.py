b381755e6db7d9ffcd2d566e7a585ba5eac5d96c
WistfulHopes <kp72002@gmail.com>
2024-02-05 07:57:18 -0800

    Add files via upload

--- mot/common/mot.py
+++ mot/common/mot.py
@@ -124,7 +124,7 @@ class MotRecord:
 			return None
 		else:
 			for bone in rig.data.bones:
-				if bone["ID"] == self.boneIndex:
+				if int(bone.name[-3:], 16) == self.boneIndex:
 					boneName = bone.name
 					break
 		if not boneName:
