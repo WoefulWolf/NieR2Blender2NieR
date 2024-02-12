75446daaef4dd1342d47f0431685391483869ac9
WistfulHopes <kp72002@gmail.com>
2024-02-12 12:44:24 -0800

    gbfr tpose fixer

--- mot/importer/tPoseFixer.py
+++ mot/importer/tPoseFixer.py
@@ -1,11 +1,36 @@
 import bpy
 
+TPOSE_BONES = ['_006', '_a06', '_00a', '_a0a']
+
+def get_parent_object(obj):
+	if obj.name in TPOSE_BONES:
+		return True
+
+	parent = obj.parent
+    
+	if parent != None and parent.name not in TPOSE_BONES:
+		get_parent_object(parent)
+	elif parent == None:
+		return False
+	else:
+		return True
+        
 FIXED_FLAG = "FIXED_T-POSE"
 def fixTPose(armObj: bpy.types.Object):
 	if FIXED_FLAG in armObj and armObj[FIXED_FLAG]:
 		return
 	print("Applying T-Pose")
 	
+	# apply armature as rest pose
+	bpy.context.view_layer.objects.active = armObj
+	armObj.select_set(True)
+	bpy.ops.object.mode_set(mode="POSE")
+	for pbone in armObj.pose.bones:
+		if get_parent_object(pbone):
+			pbone.rotation_quaternion = pbone['TPose_Rotation']
+	
+	bpy.ops.object.mode_set(mode="OBJECT")
+    
 	# apply armature modifiers on all armature children
 	child: bpy.types.Object
 	for child in armObj.children:
@@ -16,11 +41,11 @@ def fixTPose(armObj: bpy.types.Object):
 				continue
 			bpy.context.view_layer.objects.active = child
 			bpy.ops.object.modifier_apply(modifier=mod.name)
-	
-	# apply armature as rest pose
+
 	bpy.context.view_layer.objects.active = armObj
 	armObj.select_set(True)
 	bpy.ops.object.mode_set(mode="POSE")
+    
 	bpy.ops.pose.armature_apply()
 	bpy.ops.object.mode_set(mode="OBJECT")
 	
